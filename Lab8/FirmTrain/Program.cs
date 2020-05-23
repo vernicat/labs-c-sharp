225074c7530b4ac0037d5e1e8ab1d3036f4536c3
vernicat <fkofte@mail.ru>
2020-05-24 01:56:57 +0300

    Add files via upload

---
+++ Lab8/FirmTrain/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace FirmTrain
+{
+    class Program
+    {
+        class Train
+        {
+            public string number, message;
+            public int count;
+
+            public Train(string a, string b, int c)
+            {
+                this.number = a;
+                this.message = b;
+                this.count = c;
+            }
+
+            public override string ToString()
+            {
+                return "Поезд " + number + " " + message + ".\nКоличество вагонов: " + count + ".";
+            }
+        }
+
+        class FirmTrain : Train
+        {
+            public string nameFirm;
+            public double kPrice;
+
+            public FirmTrain(string number, string message, int count, string nameFirm, double kPrice) : base(number, message, count)
+            {
+                this.number = number;
+                this.message = message;
+                this.count = count;
+                this.nameFirm = nameFirm;
+                this.kPrice = kPrice;
+            }
+            public override string ToString()
+            {
+            return "Поезд " + number + " " + message + ".\nКоличество вагонов: " + count + ".\nНазвание фирменного поезда: " + nameFirm + ".\nКоэффициент удорожания: " + kPrice;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Train a = new FirmTrain("6017", "отправляется с 5 пути", 16, "РЖД", 2.5);
+            Console.WriteLine(a.ToString());
+        }
+    }
+}
